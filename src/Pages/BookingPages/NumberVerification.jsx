8a315ca50e256e7f12d15d842bee6f1ab1c1ebae
Jasil-kk <jasikk2522@gmail.com>
2024-05-29 14:41:01 +0530

    number verification page ui started

--- src/Pages/BookingPages/NumberVerification.jsx
+++ src/Pages/BookingPages/NumberVerification.jsx
@@ -10,22 +10,37 @@ const NumberVerification = () => {
       <Spacing lg={120} md={60} />
       <div className="container mt-5">
         <CustomStepper activeStep={1} completedSteps={[]} />
+        <Spacing lg={50} md={20} />
         <div className="booking_container">
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">
-              +91
-            </span>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
+          <div className="booking_form_card">
+            <form className="booking_form">
+              <Spacing lg={100} md={50} />
+              <h3 className="booking_form_card_title">Verify your number</h3>
+              <Spacing lg={15} md={10} />
+              <div class="input-group">
+                <span class="input-group-text" id="basic-phone">
+                  +91
+                </span>
+                <input
+                  type="number"
+                  class="form-control"
+                  placeholder="Phone"
+                  aria-label="Phone"
+                  aria-describedby="basic-phone"
+                />
+              </div>
+              <Spacing lg={40} md={30} />
+              <div className="booking_form_card_btn_wrapper">
+                <button
+                  className="booking_form_card_btn"
+                  onClick={() => navigate("/booking/otp-verification")}
+                >
+                  Verify
+                </button>
+              </div>
+              <Spacing lg={80} md={40} />
+            </form>
           </div>
-          <button onClick={() => navigate("/booking/otp-verification")}>
-            Verify
-          </button>
         </div>
       </div>
       <Spacing lg={120} md={80} />
