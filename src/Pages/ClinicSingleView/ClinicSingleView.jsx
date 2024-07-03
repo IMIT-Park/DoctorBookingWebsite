53862cbf8a908b8a8e49fafc34452ce637c6c32e
Simiksunny <simiksunny4@gmail.com>
2024-07-03 17:55:18 +0530

    clinicsingleview

--- src/Pages/ClinicSingleView/ClinicSingleView.jsx
+++ src/Pages/ClinicSingleView/ClinicSingleView.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { axiosApi } from "../../axiosInstance";
 
 const ClinicSingleView = () => {
   const navigate = useNavigate();
+
+  const doctorId = "someDoctorId";
+
+  const [loading, setLoading] = useState(false);
+  const [doctorDetails, setDoctorDetails] = useState(null);
+  const [doctorClinics, setDoctorClinics] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axiosApi.get(`/v1/doctor/getalldr/${doctorId}`);
+      setDoctorDetails(response?.data?.Doctor);
+      setDoctorClinics(response?.data?.allclinics);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // fetching doctor details
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
       <div className="st-height-b120 st-height-lg-b80" />
       <div className="container">
-        {/* <div className="details_wrapper flex-row"> */}
         <div className="details_wrapper">
           <div className="profile_details_container">
-            <div className="profile_left_section">
-              {/* <div className="profile_container"></div> */}
+            <div className="clinic_card">
               <div className="image-column">
                 <img
                   src="https://via.placeholder.com/600"
@@ -44,7 +70,7 @@ const ClinicSingleView = () => {
             </div>
           </div>
 
-          <div className="doctor_title mt-4">Doctors List</div>
+          <div className="doctor_title mt-5">Doctors List</div>
           <div className=" doctor_list_card mt-3">
             <div className=" doctor_list_card1 flex-row doctorlist_details_wrapper1">
               <img
@@ -72,120 +98,15 @@ const ClinicSingleView = () => {
                   <div className="day">SAT</div>
                 </div>
 
-                {/* <div className="booking_form_card_btn_wrapper"> */}
                 <button className="clinic_doctor_card_btn mt-3">
                   Book Appointment
                 </button>
-                {/* </div> */}
               </div>
             </div>
-            <div className=" doctor_list_card1">
-              <div className=" doctor_list_card1 flex-row doctorlist_details_wrapper1">
-                <img
-                  src="https://via.placeholder.com/600"
-                  alt="Clinic"
-                  style={{
-                    width: "150px",
-                    height: "200px",
-                    margin: "8px",
-                    borderRadius: "5px",
-                  }}
-                />
-                <div className="doctor_detailbox">
-                  <h2 className="clini_doctor_name mb-1">Dr. Asha Sunny</h2>
-                  <p className="docotr_job mb-1">MBBS</p>
-                  <p className="doctor_surgery">General Surgery</p>
-
-                  <div className="time_selector_btn1">
-                    <div className="day">SUN</div>
-                    <div className="day">MON</div>
-                    <div className="day">TUE</div>
-                    <div className="day">WED</div>
-                    <div className="day">THU</div>
-                    <div className="day">FRI</div>
-                    <div className="day">SAT</div>
-                  </div>
-
-                  {/* <div className="booking_form_card_btn_wrapper"> */}
-                  <button className="clinic_doctor_card_btn mt-3">
-                    Book Appointment
-                  </button>
-                  {/* </div> */}
-                </div>
-              </div>
-            </div>
+            
           </div>
 
-          <div className=" doctor_list_card mt-3">
-            <div className=" doctor_list_card1 flex-row doctorlist_details_wrapper1">
-              <img
-                src="https://via.placeholder.com/600"
-                alt="Clinic"
-                style={{
-                  width: "150px",
-                  height: "200px",
-                  margin: "8px",
-                  borderRadius: "5px",
-                }}
-              />
-              <div className="doctor_detailbox">
-                <h2 className="clini_doctor_name mb-1">Dr. Tomson</h2>
-                <p className="docotr_job mb-1">MBBS</p>
-                <p className="doctor_surgery">General Surgery</p>
-
-                <div className="time_selector_btn1">
-                  <div className="day">SUN</div>
-                  <div className="day">MON</div>
-                  <div className="day">TUE</div>
-                  <div className="day">WED</div>
-                  <div className="day">THU</div>
-                  <div className="day">FRI</div>
-                  <div className="day">SAT</div>
-                </div>
-
-                {/* <div className="booking_form_card_btn_wrapper"> */}
-                <button className="clinic_doctor_card_btn mt-3">
-                  Book Appointment
-                </button>
-                {/* </div> */}
-              </div>
-            </div>
-            <div className=" doctor_list_card1">
-              <div className=" doctor_list_card1 flex-row doctorlist_details_wrapper1">
-                <img
-                  src="https://via.placeholder.com/600"
-                  alt="Clinic"
-                  style={{
-                    width: "150px",
-                    height: "200px",
-                    margin: "8px",
-                    borderRadius: "5px",
-                  }}
-                />
-                <div className="doctor_detailbox">
-                  <h2 className="clini_doctor_name mb-1">Dr. Praveena</h2>
-                  <p className="docotr_job mb-1">MBBS, MD</p>
-                  <p className="doctor_surgery">General Surgery</p>
-
-                  <div className="time_selector_btn1">
-                    <div className="day">SUN</div>
-                    <div className="day">MON</div>
-                    <div className="day">TUE</div>
-                    <div className="day">WED</div>
-                    <div className="day">THU</div>
-                    <div className="day">FRI</div>
-                    <div className="day">SAT</div>
-                  </div>
-
-                  {/* <div className="booking_form_card_btn_wrapper"> */}
-                  <button className="clinic_doctor_card_btn mt-3">
-                    Book Appointment
-                  </button>
-                  {/* </div> */}
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
       <div className="st-height-b120 st-height-lg-b90" />
