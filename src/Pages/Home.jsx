6ea61c38b410849d9b5e194d78918c2cc3dc3675
Jasil-kk <jasikk2522@gmail.com>
2024-07-11 12:25:29 +0530

    location url changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -500,7 +500,7 @@ const brandData = [
 ];
 
 const mapLocationURL =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3924.9150083458253!2d76.2147084758732!3d10.348680866936158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7f768249c9bf5%3A0x48c8065261fbf396!2sIMIT%20PARK%20-%20International%20Media%20and%20Information%20Technology%20Park%20%7C%20IT%20Company%20In%20Kerala!5e0!3m2!1sen!2sin!4v1719316818259!5m2!1sen!2sin";
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.0361360884!2d-74.30933863574961!3d40.697539951885474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1720680729045!5m2!1sen!2sin";
 
 const Home = () => {
   const { setBookingDetails } = useContext(UserContext);
